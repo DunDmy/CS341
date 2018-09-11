5b2536de8d4aa9cacdb88763808375628fc49632
Dmytro <dundukovdmytro@gmail.com>
2018-09-10 21:13:46 -0500

    commit ReduxLib/TachyonLib/

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// added Store object from redux lib 9/10/2018 did
+// added Provider and connect from react-redux lib 9/10/2018
+// added Tachyons lib in place of CSS 9/10/2018
+import { Provider, connect } from 'react-redux';
+import { createStore} from 'redux';
+import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
